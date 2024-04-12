de40293fece08e5b4c9ee4dcc573bed6d704cc9a
Garmannen <emil.rammal@elev.ga.lbs.se>
2024-04-12 11:31:44 +0200

    inget funkar

--- Väldigt cool Gregor/Assets/Ladder.cs
+++ Väldigt cool Gregor/Assets/Ladder.cs
@@ -2,38 +2,48 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.UIElements;
 
 public class Ladder : MonoBehaviour
 {
 
     [SerializeField] GameObject playerPrefab;
 
-    Vector2 howFastClimb;
+    Vector2 playerPosY;
 
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerPosY = playerPrefab.transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
-    }
 
-    private void OnCollisionStay2D(Collision2D collision)
-    {
         
-        if (Input.GetKeyDown("W"))
+
+        if (Input.GetKey(KeyCode.W))
         {
 
-            howFastClimb = new Vector2(playerPrefab.transform.position.x, playerPrefab.transform.position.y);
-
-            howFastClimb.y = howFastClimb.y + 0.5f;
+            playerPosY = new Vector2(0, 0.2f);
 
         }
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        //if ()
+        //{
+        //    Debug.Log("Trycka W");
+
+
+        //    howFastClimb = new Vector2(playerPrefab.transform.position.x, playerPrefab.transform.position.y);
+
+        //    howFastClimb.y = howFastClimb.y + 2.5f;
+
+        //}
 
     }
 
