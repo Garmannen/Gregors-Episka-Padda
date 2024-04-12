7c9c3a6cb1a089631f3f728b1b4090c871d13ff3
Ballibab <150425297+Ballibab@users.noreply.github.com>
2024-04-12 10:34:49 +0200

    bleeeh:D

--- Väldigt cool Gregor/Assets/Player_Health.cs
+++ Väldigt cool Gregor/Assets/Player_Health.cs
@@ -2,19 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PersistantData : MonoBehaviour
+public class Player_Health : MonoBehaviour
 {
-    public int PlayerCoins;
+    [SerializeField] private int playerHealth;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+       
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (playerHealth <= 0)
+        {
+            //Restarta spelet
+        }
     }
 }
